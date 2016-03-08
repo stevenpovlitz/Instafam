8ee310b785b4008577440a05087de32af9efce4b
Steven Povlitz <steven.povlitz@gmail.com>
2016-03-08 00:29:20 -0500

    Initial Commit

---
+++ InstaFam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaFam
+//
+//  Created by Steven Povlitz on 3/8/16.
+//  Copyright © 2016 Steven Povlitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
